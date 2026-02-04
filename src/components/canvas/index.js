8ebd74294e31b568026b6af19615f625313212f9
Jubi <149138229+kafton@users.noreply.github.com>
2026-02-04 11:42:54 +0400

    Add files via upload

---
+++ src/components/canvas/index.js
@@ -0,0 +1,11 @@
+import EarthCanvas from "./Earth";
+import BallCanvas from "./Ball";
+import ComputersCanvas from "./Computers";
+import StarsCanvas from "./Stars";
+import MagicCanvas from "./Magic";
+import RobotCanvas from "./Robot";
+
+import "./magic_logic";
+
+
+export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas, MagicCanvas, RobotCanvas };
